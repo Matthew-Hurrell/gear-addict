e0873bac8e730f9ca0b66c59d4dfd20657032c26
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-04-30 20:53:40 +0000

    Import NavBar component into App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import NavBar from './components/NavBar';
+
 
 function App() {
   return (
     <div className="App">
-      <h1 className='underline text-red-600'>Gear Addict</h1>
+      <NavBar />
     </div>
   );
 }
