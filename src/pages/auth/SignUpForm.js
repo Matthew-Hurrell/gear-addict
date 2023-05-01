4b24f0076b5b90e070dba43df990ad263ef27bf7
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-01 21:00:25 +0000

    Add form to SignUpForm page

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import header from '../../assets/gear-addict-page-header.jpg';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 const SignUpForm = () => {
   return (
@@ -19,10 +20,47 @@ const SignUpForm = () => {
         <div className='h-1 bg-amber-400'></div>
 
         {/* Form Section */}
-        <div className='px-5 bg-slate-100'>
-          <div className='container mx-auto'>
-            <div></div>
-            <div></div>
+        <div className='px-5 bg-slate-100 py-24'>
+          <div className='container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-7'>
+
+            {/* Sign Up Form */}
+            <div>
+            <form className='bg-white border-zinc-400 border-2'>
+              <div>
+                <label>
+                  Username:
+                  <input type="text" name="username" />
+                </label>
+              </div>
+              <div>
+                <label>
+                  Email Address:
+                  <input type="email" name="email" />
+                </label>
+              </div>
+              <div>
+                <label>
+                  Password:
+                  <input type="password" name="password" />
+                </label>
+              </div>
+              <div>
+                <label>
+                  Confirm Password:
+                  <input type="password" name="password2" />
+                </label>
+              </div>
+              <p>Already have an account? 
+                <Link className="ml-1" to="/signin">Sign In</Link>
+              </p>
+              <input type="submit" value="Submit" />
+            </form>
+            </div>
+
+            {/* Image */}
+            <div>
+
+            </div>
           </div>
         </div>
 
