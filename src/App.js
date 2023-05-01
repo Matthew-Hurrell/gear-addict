2267a129da23153051210324fb6499a8969d8f57
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-01 17:13:41 +0000

    Add SignUpForm element into App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import "./api/axiosDefaults";
+import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <div className='container mx-auto'>
+      <div>
         <Switch>
           <Route exact path="/" render={() => <h1>Home page</h1>} />
           <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
-          <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </div>
