0076be8d88e2bbadc97ff619fe5906855ab5e3a5
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-01 15:54:45 +0000

    Add Switch and Route paths to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import NavBar from './components/NavBar';
-
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <div className='container mx-auto'>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+        </Switch>
+      </div>
     </div>
   );
 }
