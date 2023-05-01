4d7e4ed1ffb4d53fe16c699a087399559a7c46b2
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-01 19:48:59 +0000

    Add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <section className='bg-zinc-800 flex items-center justify-center relative text-white'>
+
+        {/* Heading and Nav Menu */}
+        <div className='p'>
+            <h3 className='text-3xl'>Gear Addict</h3>
+            <p>Discover, organise and elevate your sound</p>
+        </div>
+
+        {/* Flame Logo */}
+        <div></div>
+
+        {/* Copyright and Socials */}
+        <div></div>
+    </section>
+  )
+}
+
+export default Footer
