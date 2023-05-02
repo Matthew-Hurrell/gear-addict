c36e097a4240253147926331fe2b5537974df1de
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-02 21:10:17 +0000

    Add SignUpForm handleSubmit, handleChange and error handling

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import header from '../../assets/gear-addict-page-header.jpg';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { Link, useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import image from '../../assets/gear-addict-band-jump.jpg';
+import axios from 'axios';
 
 const SignUpForm = () => {
+  const [signUpData, setSignUpData] = useState({
+    username: '',
+    password1: '',
+    password2: '',
+  });
+  const { username, password1, password2 } = signUpData;
+
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignUpData({
+      ...signUpData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post('/dj-rest-auth/registration/', signUpData);
+      history.push('/signin');
+    } catch(err) {
+      setErrors(err.response?.data);
+      console.log(err.response?.data);
+    }
+  };
+
   return (
     <div>
 
@@ -25,32 +55,62 @@ const SignUpForm = () => {
           <div className='container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-7'>
 
             {/* Sign Up Form */}
-            <div class="2xl:w-10/12 2xl:ml-auto">
-              <form className='bg-zinc-800 h-full pt-10 pb-8 px-8 md:pt-16 md:pb-14 md:px-16 lg:px-12 xl:px-16 border-2 border-amber-400 shadow-xl text-left flex flex-col justify-center gap-5'>
+            <div className="2xl:w-10/12 2xl:ml-auto">
+              <form 
+                className='bg-zinc-800 h-full pt-10 pb-8 px-8 md:pt-16 md:pb-14 md:px-16 lg:px-12 xl:px-16 border-2 border-amber-400 shadow-xl text-left flex flex-col justify-center gap-7'
+                onSubmit={handleSubmit}
+              >
                 <div>
                   <label className='flex flex-col sm:flex-row justify-between items-center text-white font-bold'>
                     Username:
-                    <input className='mt-1 sm:mt-0 border-zinc-400 border-2 w-full sm:w-7/12 px-2 py-0.5 font-normal' type="text" name="username" placeholder='MrMetalHead' />
+                    <input 
+                      className='mt-1 sm:mt-0 border-zinc-400 border-2 w-full sm:w-7/12 px-2 py-0.5 font-normal text-black'
+                      type="text"
+                      name="username"
+                      placeholder='MrMetalHead'
+                      value={username}
+                      onChange={handleChange}
+                    />
                   </label>
                 </div>
-                <div>
-                  <label className='flex flex-col sm:flex-row justify-between items-center text-white font-bold'>
-                    Email Address:
-                    <input className='mt-1 sm:mt-0 border-zinc-400 border w-full sm:w-7/12 px-2 py-0.5 font-normal' type="email" name="email" placeholder='mrmetalhead@example.com' />
-                  </label>
-                </div>
+                {errors.username?.map((message, idx) => 
+                  <p className='text-red-500' key={idx}>{message}</p>
+                )}
+
                 <div>
                   <label className='flex flex-col sm:flex-row justify-between items-center text-white font-bold'>
                     Password:
-                    <input className='mt-1 sm:mt-0 border-zinc-400 border w-full sm:w-7/12 px-2 py-0.5 font-normal' type="password" name="password" placeholder='Ilovemetal123' />
+                    <input
+                      className='mt-1 sm:mt-0 border-zinc-400 border w-full sm:w-7/12 px-2 py-0.5 font-normal text-black'
+                      type="password"
+                      name="password1"
+                      placeholder='Ilovemetal123'
+                      value={password1}
+                      onChange={handleChange}
+                    />
                   </label>
                 </div>
+                {errors.password1?.map((message, idx) => 
+                  <p className='text-red-500' key={idx}>{message}</p>
+                )}
+
                 <div>
                   <label className='flex flex-col sm:flex-row justify-between items-center text-white font-bold'>
                     Confirm Password:
-                    <input className='mt-1 sm:mt-0 border-zinc-400 border w-full sm:w-7/12 px-2 py-0.5 font-normal' type="password" name="password2" placeholder='Ilovemetal123' />
+                    <input
+                      className='mt-1 sm:mt-0 border-zinc-400 border w-full sm:w-7/12 px-2 py-0.5 font-normal text-black'
+                      type="password"
+                      name="password2"
+                      placeholder='Ilovemetal123'
+                      value={password2}
+                      onChange={handleChange}
+                    />
                   </label>
                 </div>
+                {errors.password2?.map((message, idx) => 
+                  <p className='text-red-500' key={idx}>{message}</p>
+                )}
+
                 <div className='flex flex-col sm:flex-row items-center justify-between text-white mt-2'>
                   <p className='mb-1 text-base md:text-lg'>Already have an account? 
                     <Link className="text-base md:text-lg text-center ml-1 sm:ml-1.5 hover:underline underline-offset-4 decoration-2 text-amber-500" to="/signin">Sign In</Link>
@@ -62,11 +122,14 @@ const SignUpForm = () => {
                     Sign up
                   </button>
                 </div>
+                {errors.non_field_errors?.map((message, idx) => 
+                  <p className='text-red-500' key={idx}>{message}</p>
+                )}
               </form>
             </div>
 
             {/* Image */}
-            <div class="2xl:w-10/12">
+            <div className="2xl:w-10/12">
               <img className='object-center object-cover w-full h-full' src={image} />
             </div>
           </div>
