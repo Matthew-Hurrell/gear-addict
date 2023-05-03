4d530e1a79bd2dcfad775b54b70267c01f37e44c
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-03 20:12:54 +0000

    Add alt tag to hero image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,11 @@ const Hero = (props) => {
 
     return (
         <section className='h-52 md:h-72 lg:h-[400px] relative flex justify-center sm:justify-end items-center'>
-            <img src={header} className='z-0 absolute top-0 object-top object-cover w-full h-full grayscale brightness-200' />
+            <img 
+                src={header} 
+                className='z-0 absolute top-0 object-top object-cover w-full h-full grayscale brightness-200'
+                alt="Black and white fender amplifier with drumkit in the background" 
+            />
             <div className='z-10 bg-amber-500/[.9] relative inline-block px-10 py-5 h-full flex justify-center items-center sm:mr-36'>
                 <h1 className='text-white text-4xl md:text-5xl opacity-100'>
                     {title} <i className="fa-solid fa-fire-flame text-4xl"></i>
