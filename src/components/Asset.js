e8d24f0e041c39a7f50a402d6d8844ca0f65b1e0
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-08 10:28:07 +0000

    Create asset component with react spinner

---
+++ src/components/Asset.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import FadeLoader from "react-spinners/FadeLoader";
+
+
+const Asset = ({spinner, src, message}) => {
+  const [loading, setLoading] = useState(true);
+  const [color, setColor] = useState("#fbbf24");
+
+  return (
+        <div className="sweet-loading">
+            {/* <button onClick={() => setLoading(!loading)}>Toggle Loader</button> */}
+
+            {spinner && <FadeLoader
+                color={color}
+                loading={loading}
+                size={50}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+            />}
+            {src && <img src={src} alt={message} />}
+            {message && <p className="">{message}</p>}
+        </div>
+    );
+};
+
+export default Asset;
