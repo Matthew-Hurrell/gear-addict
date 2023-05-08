5985e1df41aac2d07bca845447b39acfcc42d1d5
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-08 17:42:58 +0000

    Add routes and profile id filtering to app.js for rigs

--- src/App.js
+++ src/App.js
@@ -8,18 +8,48 @@ import SignInForm from './pages/auth/SignInForm';
 import RigCreateForm from './pages/rigs/RigCreateForm';
 import RigPage from './pages/rigs/RigPage';
 import RigsPage from './pages/rigs/RigsPage';
+import { useCurrentUser } from './contexts/CurrentUserContext';
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
 
   return (
     <div className="App">
       <NavBar />
       <div>
         <Switch>
-          <Route exact path="/" render={() => <RigsPage message="No results found! Adjust your search"/>} />
+          <Route 
+            exact 
+            path="/" 
+            render={() => (
+              <RigsPage message="No results found! Adjust your search"/>
+            )} 
+          />
+          <Route 
+            exact 
+            path="/feed" 
+            render={() => (
+              <RigsPage 
+                message="No results found! Adjust your search or become a fan of another user to see their rigs here" 
+                filter={`owner__idolguy__fan__profile=${profile_id}&`}
+              />
+            )} 
+          />
+          <Route 
+            exact 
+            path="/saved" 
+            render={() => (
+              <RigsPage 
+                message="No results found! Adjust your search or save a rig to store them here" 
+                filter={`stars__owner__profile=${profile_id}&`}
+              />
+            )} 
+          />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/rig/create" render={() => <RigCreateForm />} />
+          <Route exact path="/rigs" render={() => <RigsPage filter={`owner__profile=${profile_id}&`} />} />
           <Route exact path="/rigs/:id" render={() => <RigPage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
