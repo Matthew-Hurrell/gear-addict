8c64035541da83f0f0b25cd53c4ea485242f5224
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-08 17:24:12 +0000

    Add basic rigs page template

---
+++ src/pages/rigs/RigsPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RigsPage = () => {
+  return (
+    <section className='bg-slate-100 px-5 py-12 lg:py-24'>
+        <div className='container mx-auto'>
+
+        </div>
+    </section>
+  )
+}
+
+export default RigsPage
