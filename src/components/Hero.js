580bad900ab449bb0a34b9ba326cb860e1c68ab3
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-08 12:29:19 +0000

    Add different image functionality to hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import header from '../assets/gear-addict-page-header.jpg';
+import Righeader from '../assets/gear-addict-hero-2.jpg';
 
 const Hero = (props) => {
 
-    const {title} = props;
+    const {title, righeader} = props;
 
     return (
         <section className='h-52 md:h-72 lg:h-[400px] relative flex justify-center sm:justify-end items-center'>
-            <img 
-                src={header} 
-                className='z-0 absolute top-0 object-top object-cover w-full h-full grayscale brightness-200'
-                alt="Black and white fender amplifier with drumkit in the background" 
-            />
+            {righeader ? (
+                    <img 
+                        src={Righeader}
+                        className='z-0 absolute top-0 object-center object-cover w-full h-full grayscale'
+                        alt="Black and white fender amplifier" 
+                />
+                ) : (
+                    <img
+                        src={header}
+                        className='z-0 absolute top-0 object-top object-cover w-full h-full grayscale brightness-200'
+                        alt="Black and white fender amplifier with drumkit in the background" 
+                    />
+                )
+            }
             <div className='z-10 bg-amber-500/[.9] relative inline-block px-10 py-5 h-full flex justify-center items-center sm:mr-36'>
                 <h1 className='text-white text-4xl md:text-5xl opacity-100'>
                     {title} <i className="fa-solid fa-fire-flame text-4xl"></i>
