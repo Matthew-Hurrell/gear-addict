d4228186d08be60986351fe5aee4736235171d8b
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-11 19:47:50 +0000

    Import components to home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,22 +1,44 @@
-import React from 'react'
-import RigsPage from './rigs/RigsPage'
-import HomeHero from '../components/HomeHero'
-import SearchBar from '../components/SearchBar'
-import IntroHome from '../components/IntroHome'
-import HowToUse from '../components/HowToUse'
+import React from 'react';
+import RigsPage from './rigs/RigsPage';
+import HomeHero from '../components/HomeHero';
+import SearchBar from '../components/SearchBar';
+import IntroHome from '../components/IntroHome';
+import HowToUse from '../components/HowToUse';
+import Seperator from '../components/Seperator';
+import IconSeperator from '../components/IconSeperator';
 
 const HomePage = () => {
   return (
     <div>
+
+        {/* Hero */}
         <HomeHero />
+
+        {/* Seperator */}
+        <Seperator />
+
+        {/* Search Bar */}
         <SearchBar />
+
+        {/* Introduction */}
         <IntroHome />
+
+        {/* Icon Seperator */}
+        <IconSeperator />
+
+        {/* How To Use Section */}
         <HowToUse />
+
+        {/* Icon Seperator */}
+        <IconSeperator />
+
+        {/* Hottest Rigs */}
         <RigsPage 
             message="No results found! Adjust your search"
             filter={`&ordering=likes__created_at&`}
             title="Hottest Rigs"
         />
+
     </div>
   )
 }
