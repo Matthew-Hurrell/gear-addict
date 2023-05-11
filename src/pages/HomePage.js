334e9a0b24a40073d1f076facd36d66bac84f025
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-11 18:51:37 +0000

    Import new components into home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import RigsPage from './rigs/RigsPage'
 import HomeHero from '../components/HomeHero'
+import SearchBar from '../components/SearchBar'
+import IntroHome from '../components/IntroHome'
+import HowToUse from '../components/HowToUse'
 
 const HomePage = () => {
   return (
     <div>
         <HomeHero />
+        <SearchBar />
+        <IntroHome />
+        <HowToUse />
         <RigsPage 
             message="No results found! Adjust your search"
             filter={`&ordering=likes__created_at&`}
