55d9f31684f6bc1dddf6727ab4ef792b4fd8e6e9
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-11 18:50:40 +0000

    Add responsive style changes to rigs page title

--- src/pages/rigs/RigsPage.js
+++ src/pages/rigs/RigsPage.js
@@ -26,7 +26,7 @@ const RigsPage = ({ message, filter="", title }) => {
     return (
         <section className='bg-slate-100 px-5 py-12 lg:py-24'>
 
-            {title && <h2 className='text-center text-4xl text-black mb-14'>{title}</h2>}
+            {title && <h2 className='text-center text-3xl lg:text-4xl text-black mb-14'>{title}</h2>}
 
             <div className='container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10'>
                 {hasLoaded ? (
