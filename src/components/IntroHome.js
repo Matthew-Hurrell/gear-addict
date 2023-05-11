0d5ea5ee48eebb7aa675a1e4d131f874a6568a55
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-11 19:05:38 +0000

    Add basic content and styling for intro section

--- src/components/IntroHome.js
+++ src/components/IntroHome.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 const IntroHome = () => {
   return (
-    <div>IntroHome</div>
+    <section className='bg-white px-5 py-12 lg:py-24'>
+        <div className='container mx-auto'>
+            <h2 className='text-3xl lg:text-4xl mb-2'>Welcome to Gear Addict</h2>
+            <h3 className='text-lg lg:text-xl mb-7'>Home of music gear fans and enthusiasts</h3>
+                <p class="max-w-4xl mx-auto text-base lg:text-lg">
+                    Are you a musician who loves to collect and play with different instruments and equipment?
+                    Do you find yourself constantly searching for the right gear for your performances and recordings?
+                    If so, Gear Addict is the perfect platform for you. Our site allows you to create a profile,
+                    add and catalogue your music gear, and organise it according to your preferences.
+                    You can also showcase your live performance gear set ups, share them with other users,
+                    and discover new ideas and inspirations from fellow musicians. With Gear Addict, you can 
+                    focus on what matters most - making music and enjoying the gear that fuels your creativity.
+                </p>
+        </div>
+    </section>
   )
 }
 
