69ad9f013a5764f3530f59b0e34232bb76c5accc
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-12 17:16:50 +0000

    Add slice variable to rigs page component to limit instances of rigs rendered

--- src/pages/rigs/RigsPage.js
+++ src/pages/rigs/RigsPage.js
@@ -5,10 +5,11 @@ import RigCard from './RigCard';
 import Asset from '../../components/Asset';
 import BrokenInstruments from '../../assets/gear-addict-broken-instruments.jpeg';
 
-const RigsPage = ({ message, filter="", title, query }) => {
+const RigsPage = ({ message, filter="", title, slice_num }) => {
     const [rigs, setRigs] = useState({ results: [] });
     const [hasLoaded, setHasLoaded] = useState(false);
     const { pathname } = useLocation();
+    const [query, setQuery] = useState("");
 
     useEffect(() => {
         const fetchRigs = async () => {
@@ -16,7 +17,6 @@ const RigsPage = ({ message, filter="", title, query }) => {
                 const {data} = await axiosReq.get(`/rigs/?${filter}search=${query}`);
                 setRigs(data);
                 setHasLoaded(true);
-                console.log(query);
             } catch(err) {
                 console.log(err);
             }
@@ -40,9 +40,15 @@ const RigsPage = ({ message, filter="", title, query }) => {
             {hasLoaded ? (  
                 <div className='container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10'>
                     {rigs.results.length ? (
-                        rigs.results.map(rig => (
-                            <RigCard key={rig.id} {...rig} setRigs={setRigs} />
-                        ))
+                        slice_num ? (
+                            rigs.results.slice(0, slice_num).map(rig => (
+                                <RigCard key={rig.id} {...rig} setRigs={setRigs} />
+                            ))
+                        ) : (
+                            rigs.results.map(rig => (
+                                <RigCard key={rig.id} {...rig} setRigs={setRigs} />
+                            ))
+                        )
                     ) : (
                         <>
                             <div className='bg-zinc-800 flex justify-center items-center py-10 px-10'>
