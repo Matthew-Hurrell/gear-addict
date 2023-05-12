e020cf0940b950d993b5944f5cb897928418f23e
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-12 18:36:28 +0000

    Add infinite scroll functionality with logic to rigs page

--- src/pages/rigs/RigsPage.js
+++ src/pages/rigs/RigsPage.js
@@ -4,6 +4,8 @@ import { axiosReq } from '../../api/axiosDefaults';
 import RigCard from './RigCard';
 import Asset from '../../components/Asset';
 import BrokenInstruments from '../../assets/gear-addict-broken-instruments.jpeg';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { fetchMoreData } from '../../utils/utils';
 
 const RigsPage = ({ message, filter="", title, slice_num, query }) => {
     const [rigs, setRigs] = useState({ results: [] });
@@ -16,6 +18,9 @@ const RigsPage = ({ message, filter="", title, slice_num, query }) => {
                 const {data} = await axiosReq.get(`/rigs/?${filter}search=${query}`);
                 setRigs(data);
                 setHasLoaded(true);
+                // console.log(filter);
+                // console.log(`/rigs/?${filter}search=${query}`);
+                // console.log(data);
             } catch(err) {
                 console.log(err);
             }
@@ -37,29 +42,40 @@ const RigsPage = ({ message, filter="", title, slice_num, query }) => {
             {title && <h2 className='text-center text-3xl lg:text-4xl text-black mb-14'>{title}</h2>}
 
             {hasLoaded ? (  
-                <div className='container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10'>
+                <>
                     {rigs.results.length ? (
-                        slice_num ? (
-                            rigs.results.slice(0, slice_num).map(rig => (
-                                <RigCard key={rig.id} {...rig} setRigs={setRigs} />
-                            ))
-                        ) : (
-                            rigs.results.map(rig => (
-                                <RigCard key={rig.id} {...rig} setRigs={setRigs} />
-                            ))
-                        )
+                        <InfiniteScroll 
+                            children={
+                                slice_num ? (
+                                    rigs.results.slice(0, slice_num).map(rig => (
+                                        <RigCard key={rig.id} {...rig} setRigs={setRigs} />
+                                    ))
+                                ) : (
+                                    rigs.results.map(rig => (
+                                        <RigCard key={rig.id} {...rig} setRigs={setRigs} />
+                                    ))
+                                )
+                            }
+                            dataLength={rigs.results.length}
+                            loader={<Asset spinner />}
+                            hasMore={!!rigs.next}
+                            next={() => fetchMoreData(rigs, setRigs)}
+                            className='container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10'
+                        />
                     ) : (
-                        <>
+                        <div className='container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10'>
                             <div className='bg-zinc-800 flex justify-center items-center py-10 px-10'>
                                 <h2 className='text-white text-2xl'>{message}</h2>
                             </div>
                             <Asset src={BrokenInstruments} alt="Illustration of broken instruments" />
-                        </>
+                        </div>
                     )}
-                </div>
+                </>
             ) : (
-                <div className='flex items-center justify-center'>
-                    <Asset spinner />
+                <div className='container mx-auto'>
+                    <div className='flex items-center justify-center'>
+                        <Asset spinner />
+                    </div>
                 </div>
             )}
 
