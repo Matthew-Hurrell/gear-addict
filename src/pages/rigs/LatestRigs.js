3ceac5b41e38faea5c62025ea69605642e866127
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-12 17:43:00 +0000

    Add components to latest rigs with search query state

--- src/pages/rigs/LatestRigs.js
+++ src/pages/rigs/LatestRigs.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RigsPage from './RigsPage';
 import Hero from '../../components/Hero';
+import SearchBar from '../../components/SearchBar';
+import Seperator from '../../components/Seperator';
 
 const LatestRigs = () => {
-  return (
-    <div>
-        <Hero title="Latest Rigs" />
-        <RigsPage 
-            message="No results found! Adjust your search"
-        />
-    </div>
-  )
+    const [query, setQuery] = useState("");
+
+    return (
+        <div>
+
+            {/* Hero */}
+            <Hero title="Latest Rigs" />
+
+            {/* Seperator */}
+            <Seperator />
+
+            {/* SearchBar */}
+            <SearchBar query={query} setQuery={setQuery} />
+
+            {/* RigsPage */}
+            <RigsPage 
+                message="No results found! Adjust your search"
+                query={query}
+            />
+        </div>
+    )
 }
 
 export default LatestRigs
