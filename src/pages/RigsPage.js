516b24f1f35cd75c17d53afdf8742c45b26de666
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-13 12:33:45 +0000

    Add rigspage template with components

---
+++ src/pages/RigsPage.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import Hero from '../components/Hero';
+import Seperator from '../components/Seperator';
+import SearchBar from '../components/SearchBar';
+import RigsList from './rigs/RigsList';
+
+const RigsPage = ({filter}) => {
+
+    const [query, setQuery] = useState("");
+
+    return (
+        <div>
+
+            {/* Hero */}
+            <Hero title="My Rigs"/>
+
+            {/* Seperator */}
+            <Seperator />
+
+            {/* Search Bar */}
+            <SearchBar query={query} setQuery={setQuery} />
+
+            {/* Rigs List */}
+            <RigsList query={query} filter={filter} message="No results. Adjust search or add a rig to view it here" />
+
+        </div>
+    )
+}
+
+export default RigsPage
