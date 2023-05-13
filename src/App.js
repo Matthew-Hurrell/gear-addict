4e56d387e1c70f8e14cd10b2fb50af5b65e35a4f
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-13 12:32:58 +0000

    Chnage location of rigs page import on app.js

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import Footer from './components/Footer';
 import SignInForm from './pages/auth/SignInForm';
 import RigCreateForm from './pages/rigs/RigCreateForm';
 import RigPage from './pages/rigs/RigPage';
-import RigsPage from './pages/rigs/RigsPage';
 import { useCurrentUser } from './contexts/CurrentUserContext';
 import HomePage from './pages/HomePage';
 import LatestRigs from './pages/rigs/LatestRigs';
 import FeedPage from './pages/FeedPage';
 import SavedPage from './pages/SavedPage';
+import RigsPage from './pages/RigsPage';
 
 function App() {
   const currentUser = useCurrentUser();
