da4372ead39e02a5fd567bbd7b16dde0ed044de7
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-15 20:08:30 +0000

    Add styling and content to comment component

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -12,16 +12,20 @@ const Comment = (props) => {
     } = props;
 
     return (
-        <div>
-            <article>
+        <div className='py-5'>
+            <article className='flex gap-10 items-center'>
                 <Link to={`/profiles/${profile_id}`}>
                     <Avatar src={profile_image} />
                 </Link>
-                <span>{owner}</span>
-                <span>{updated_at}</span>
-                <p>
-                    {content}
-                </p>
+                <div className='w-full'>
+                    <div className='flex flex-col lg:flex-row items-start lg:items-center justify-center lg:justify-between w-full mb-2'>
+                        <h4 className='text-lg lg:text-xl mr-10'>{owner}</h4>
+                        <p className='text-sm lg:text-base text-gray-600'>{updated_at}</p>
+                    </div>
+                    <p className='text-left text-base lg:text-lg'>
+                        {content}
+                    </p>
+                </div>
             </article>
         </div>
     )
