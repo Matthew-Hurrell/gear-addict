d600b7d4586827a76d6c78213900da8e7bdb7512
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-16 20:37:16 +0000

    Add gear page component

---
+++ src/pages/gear/GearPage.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import Hero from '../../components/Hero';
+import SearchBar from '../../components/SearchBar';
+import Seperator from '../../components/Seperator';
+import GearList from './GearList';
+
+const GearPage = ({filter}) => {
+
+    const [query, setQuery] = useState("");
+
+    return (
+        <div>
+
+            {/* Hero */}
+            <Hero title="Gear" />
+
+            {/* Seperator */}
+            <Seperator />
+
+            {/* SearchBar */}
+            <SearchBar query={query} setQuery={setQuery} gear />
+
+            {/* GearList */}
+            <GearList  query={query} filter={filter} message="No results. Adjust search or add a rig to view it here" />
+            
+        </div>
+    )
+}
+
+export default GearPage
