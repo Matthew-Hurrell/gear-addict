0b98839e39da7cad90eaa868e76adc25bd1ca077
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-16 19:15:09 +0000

    Add conditional and state for comment edit form

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Avatar from '../../components/Avatar';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import DropdownMenu from '../../components/DropdownMenu';
 import { axiosRes } from '../../api/axiosDefaults';
+import CommentEditForm from './CommentEditForm';
 
 const Comment = (props) => {
     const {
@@ -16,6 +17,8 @@ const Comment = (props) => {
         setRig,
         setComments,
     } = props;
+
+    const [showEditForm, setShowEditForm] = useState(false);
 
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner;
@@ -45,18 +48,30 @@ const Comment = (props) => {
                 <Link to={`/profiles/${profile_id}`}>
                     <Avatar src={profile_image} />
                 </Link>
-                <div className='w-full relative'>
-                    <div className='flex flex-col lg:flex-row items-start lg:gap-5 lg:items-center justify-center lg:justify-start w-full mb-2'>
-                        <h4 className='text-lg lg:text-xl'>{owner}</h4>
-                        <p className='text-sm lg:text-base text-gray-600'>{updated_at}</p>
-                        {is_owner && (
-                            <DropdownMenu handleEdit={() => {}} handleDelete={handleDelete} />
-                        )}
+                {showEditForm ? (
+                    <CommentEditForm 
+                        id={id}
+                        profile_id={profile_id}
+                        content={content}
+                        profileImage={profile_image}
+                        setComments={setComments}
+                        setShowEditForm={setShowEditForm}
+                    />
+                ) : (
+                    <div className='w-full relative'>
+                        <div className='flex flex-col lg:flex-row items-start lg:gap-5 lg:items-center justify-center lg:justify-start w-full mb-2'>
+                            <h4 className='text-lg lg:text-xl'>{owner}</h4>
+                            <p className='text-sm lg:text-base text-gray-600'>{updated_at}</p>
+                            {is_owner && !showEditForm && (
+                                <DropdownMenu handleEdit={() => setShowEditForm(true)} handleDelete={handleDelete} />
+                            )}
+                        </div>
+                        <p className='text-left text-base lg:text-lg'>
+                            {content}
+                        </p>
                     </div>
-                    <p className='text-left text-base lg:text-lg'>
-                        {content}
-                    </p>
-                </div>
+                )}
+
             </article>
         </div>
     )
