88072937f832cce4f4d3df6888a8d8a3ae778e67
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-20 14:19:41 +0000

    Add profile component with logic

---
+++ src/pages/profiles/Profile.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import Avatar from '../../components/Avatar';
+
+const Profile = ({profile}) => {
+    const {id, fan_id, image, owner} = profile;
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+
+    return (
+        <div>
+            <Link className='flex flex-col lg:flex-row' to={`/profiles/${id}`}>
+                <Avatar src={image} />
+                <p>{owner}</p>
+                {currentUser && !is_owner && (
+                    following_id ? (
+                        <button
+                            onClick={() => {}}
+                        >
+                            Unfan
+                        </button>
+                    ) : (
+                        <button
+                            onClick={() => {}}
+                        >
+                            Fan
+                        </button>
+                    )
+                )}
+            </Link>
+        </div>
+    )
+}
+
+export default Profile
