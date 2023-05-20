ec402f591c496b1d0b84dfdbcf094ec97812f1ad
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-20 14:21:14 +0000

    Remove code and replace with use profile data hook

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,35 +1,10 @@
 import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { axiosReq } from '../../api/axiosDefaults';
-import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import Asset from '../../components/Asset';
+import Profile from './Profile';
+import { useProfileData } from '../../contexts/ProfileDataContext';
 
 const PopularProfiles = () => {
-    const [profileData, setProfileData] = useState({
-        pageProfile: { results: [] },
-        popularProfiles: { results: [] },
-    });
-    const { popularProfiles } = profileData;
-    const currentUser = useCurrentUser();
-
-    useEffect(() => {
-        const handleMount = async () => {
-            try {
-                const {data} = await axiosReq.get(
-                    '/profiles/?ordering=-fans_count'
-                );
-                setProfileData(prevState => ({
-                    ...prevState,
-                    popularProfiles: data,
-                }));
-            } catch(err) {
-                console.log(err);
-            }
-        };
-
-        handleMount();
-    }, [currentUser]);
+    const { popularProfiles } = useProfileData();
 
     return (
         <section className='container'>
@@ -37,7 +12,7 @@ const PopularProfiles = () => {
               <>
                 <p>Most popular profiles</p>
                 {popularProfiles.results.slice(0, 4).map((profile) => (
-                    <p key={profile.id}>{profile.owner}</p>
+                    <Profile key={profile.id} profile={profile} />
                 ))}
               </>  
             ) : (
