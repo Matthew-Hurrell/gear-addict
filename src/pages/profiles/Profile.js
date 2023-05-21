f49e3dcd2b19bff4337221f97beab746404c7944
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-21 09:07:48 +0000

    Add minor style changes to profile buttons

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -19,14 +19,14 @@ const Profile = ({profile}) => {
                 fan_id ? (
                     <button
                         onClick={() => {}}
-                        className='text-zinc-800'
+                        className='hover:scale-105 shadow-md text-zinc-800 h-10 w-10 flex justify-center items-center border border-zinc-800 rounded-full'
                     >
                         <i className="fa-solid fa-hand-horns text-2xl"></i>
                     </button>
                 ) : (
                     <button
                         onClick={() => {}}
-                        className='shadow-md text-zinc-800 h-10 w-10 flex justify-center items-center border border-zinc-800 rounded-full'
+                        className='hover:scale-105 shadow-md text-zinc-800 h-10 w-10 flex justify-center items-center border border-zinc-800 rounded-full'
                     >
                         <i className="fa-light fa-hand-horns text-2xl"></i>
                     </button>
