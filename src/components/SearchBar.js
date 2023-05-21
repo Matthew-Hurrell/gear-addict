a04c0716ef513a981885deb79407317962e4471a
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-21 13:27:29 +0000

    Minor style changes to search bar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,12 +3,12 @@ import React from 'react';
 const SearchBar = ({query, setQuery, gear}) => {
 
   return (
-    <div className='bg-zinc-700 py-2 px-5'>
+    <div className='bg-zinc-600 py-2 px-5'>
       <div className='container mx-auto flex items-center justify-end'>
-        <form className='relative inline-block' onSubmit={(event) => event.preventDefault()}>
+        <form className='relative inline-block w-full md:w-80 lg:w-96' onSubmit={(event) => event.preventDefault()}>
           <input 
             placeholder={gear ? ( 'Search gear' ) : ( 'Search for a rig' ) }
-            className='text-lg relative z-10 px-3 py-1 w-72 lg:w-96' 
+            className='rounded-sm text-lg relative z-10 px-3 py-1 w-full md:w-80 lg:w-96' 
             type='text'
             value={query}
             onChange={(event) => setQuery(event.target.value)}
