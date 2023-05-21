02cc0b5c79cd2edbda6dbfcc97744409918ddad0
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-21 10:47:25 +0000

    Add route for username form in app.js

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import GearCreateForm from './pages/gear/GearCreateForm';
 import GearPage from './pages/gear/GearPage';
 import GearEditForm from './pages/gear/GearEditForm';
 import ProfilePage from './pages/profiles/ProfilePage';
+import UsernameForm from './pages/profiles/UsernameForm';
 
 function App() {
   const currentUser = useCurrentUser();
@@ -70,6 +71,7 @@ function App() {
           <Route exact path="/gear" render={() => <GearPage filter={`owner__profile=${profile_id}&`} />} />
           <Route exact path="/gear/:id/edit" render={() => <GearEditForm />} />
           <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
+          <Route exact path="/profiles/:id/edit/username" render={() => <UsernameForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </div>
