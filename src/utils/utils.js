32e360f59a1972209485a462298203dd90345aa9
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-23 11:52:50 +0000

    Add indentation and comments to utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,54 +1,54 @@
 import jwtDecode from "jwt-decode";
-import { axiosReq } from "../api/axiosDefaults"
+import { axiosReq } from "../api/axiosDefaults";
 
 export const fetchMoreData = async (resource, setResource) => {
     try {
-        const {data} = await axiosReq.get(resource.next);
+        const { data } = await axiosReq.get(resource.next);
         setResource(prevResource => ({
             ...prevResource,
-            next:data.next,
+            next: data.next,
             results: data.results.reduce((acc, cur) => {
                 return acc.some((accResult) => accResult.id === cur.id)
-                ? acc 
-                : [...acc, cur];
+                    ? acc
+                    : [...acc, cur];
             }, prevResource.results),
         }));
-    } catch(err) {
-
+    } catch (err) {
+        // console.log(err);
     };
 };
 
 export const fanHelper = (profile, clickedProfile, fan_id) => {
     return profile.id === clickedProfile.id
-    ?
+        ?
         {
             ...profile,
             fans_count: profile.fans_count + 1,
             fan_id,
         }
-    : profile.is_owner
-    ?
-        {
-            ...profile,
-            idols_count: profile.idols_count + 1,
-        }
-    :
-        profile;
+        : profile.is_owner
+            ?
+            {
+                ...profile,
+                idols_count: profile.idols_count + 1,
+            }
+            :
+            profile;
 }
 
 export const unfanHelper = (profile, clickedProfile) => {
     return profile.id === clickedProfile.id
-    ?
+        ?
         {
             ...profile,
             fans_count: profile.fans_count - 1,
             fan_id: null,
         }
-    : profile.is_owner
-    ?
-        { ...profile, idols_count: profile.idols_count - 1 }
-    :
-    profile;
+        : profile.is_owner
+            ?
+            { ...profile, idols_count: profile.idols_count - 1 }
+            :
+            profile;
 }
 
 export const setTokenTimestamp = (data) => {
