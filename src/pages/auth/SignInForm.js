d62eacc79d6c35cb05a60d467595f1e56565d893
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-23 11:02:29 +0000

    Add indentation and comments to auth forms

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import image from '../../assets/gear-addict-sign-in-page.jpg';
 import axios from 'axios';
@@ -10,6 +10,7 @@ import { setTokenTimestamp } from '../../utils/utils';
 
 const SignInForm = () => {
   const setCurrentUser = useSetCurrentUser();
+
   useRedirect('loggedIn');
 
   const [signInData, setSignInData] = useState({
@@ -18,9 +19,7 @@ const SignInForm = () => {
   });
 
   const { username, password } = signInData;
-
   const [errors, setErrors] = useState({});
-
   const history = useHistory();
 
   const handleChange = (event) => {
@@ -33,11 +32,11 @@ const SignInForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const {data} = await axios.post('/dj-rest-auth/login/', signInData);
+      const { data } = await axios.post('/dj-rest-auth/login/', signInData);
       setCurrentUser(data.user);
       setTokenTimestamp(data);
       history.goBack();
-    } catch(err) {
+    } catch (err) {
       setErrors(err.response?.data);
     }
   };
@@ -45,84 +44,88 @@ const SignInForm = () => {
   return (
     <div>
 
-        {/* Hero */}
-        <Hero title='Sign in'/>
+      {/* Hero */}
+      <Hero title='Sign in' />
 
-        {/* Seperator */}
-        <Seperator />
+      {/* Seperator */}
+      <Seperator />
 
-        {/* Form Section */}
-        <div className='px-5 py-12 lg:py-24  bg-slate-100'>
-          <div className='container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-7'>
+      {/* Form Section */}
+      <div className='px-5 py-12 lg:py-24  bg-slate-100'>
+        <div className='container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-7'>
 
-            {/* Sign Up Form */}
-            <div className="2xl:w-10/12 2xl:ml-auto">
-              <form 
-                className='bg-zinc-800 h-full pt-10 pb-8 px-8 md:pt-16 md:pb-14 md:px-16 lg:px-12 xl:px-16 border-2 border-amber-400 shadow-xl text-left flex flex-col justify-center gap-9'
-                onSubmit={handleSubmit}
-              >
-                <div>
-                  <label className='flex flex-col sm:flex-row justify-between items-center text-white font-bold'>
-                    Username:
-                    <input 
-                      className='mt-1 sm:mt-0 border-zinc-400 border-2 w-full sm:w-7/12 px-2 py-0.5 font-normal text-black'
-                      type="text"
-                      name="username"
-                      placeholder='MrMetalHead'
-                      value={username}
-                      onChange={handleChange}
-                    />
-                  </label>
-                </div>
-                {errors.username?.map((message, idx) => (
-                  <p className='text-red-500' key={idx}>{message}</p>
-                ))}
+          {/* Sign Up Form */}
+          <div className="2xl:w-10/12 2xl:ml-auto">
+            <form
+              className='bg-zinc-800 h-full pt-10 pb-8 px-8 md:pt-16 md:pb-14 md:px-16 lg:px-12 xl:px-16 border-2 border-amber-400 shadow-xl text-left flex flex-col justify-center gap-9'
+              onSubmit={handleSubmit}
+            >
 
-                <div>
-                  <label className='flex flex-col sm:flex-row justify-between items-center text-white font-bold'>
-                    Password:
-                    <input
-                      className='mt-1 sm:mt-0 border-zinc-400 border w-full sm:w-7/12 px-2 py-0.5 font-normal text-black'
-                      type="password"
-                      name="password"
-                      placeholder='Ilovemetal123'
-                      value={password}
-                      onChange={handleChange}
-                    />
-                  </label>
-                </div>
-                {errors.password?.map((message, idx) => (
-                  <p className='text-red-500' key={idx}>{message}</p>
-                ))}
+              {/* Username */}
+              <div>
+                <label className='flex flex-col sm:flex-row justify-between items-center text-white font-bold'>
+                  Username:
+                  <input
+                    className='mt-1 sm:mt-0 border-zinc-400 border-2 w-full sm:w-7/12 px-2 py-0.5 font-normal text-black'
+                    type="text"
+                    name="username"
+                    placeholder='MrMetalHead'
+                    value={username}
+                    onChange={handleChange}
+                  />
+                </label>
+              </div>
+              {errors.username?.map((message, idx) => (
+                <p className='text-red-500' key={idx}>{message}</p>
+              ))}
 
-                <div className='flex flex-col sm:flex-row items-center justify-between text-white mt-2'>
-                  <p className='mb-1 text-base md:text-lg'>Don't have an account? 
-                    <Link className="text-base md:text-lg text-center ml-1 sm:ml-1.5 hover:underline underline-offset-4 decoration-2 text-amber-500" to="/signup">Sign Up</Link>
-                  </p>
-                  <button 
-                    type="submit"
-                    className='mt-2 sm:mt-0 bg-amber-500 px-3 py-2 font-bold hover:scale-105 hover:bg-transparent border-2 border-amber-500'
-                  >
-                    Sign in
-                  </button>
-                </div>
-                {errors.non_field_errors?.map((message, idx) => (
-                  <p className='text-red-500' key={idx}>{message}</p>
-                ))}
-              </form>
-            </div>
+              {/* Password */}
+              <div>
+                <label className='flex flex-col sm:flex-row justify-between items-center text-white font-bold'>
+                  Password:
+                  <input
+                    className='mt-1 sm:mt-0 border-zinc-400 border w-full sm:w-7/12 px-2 py-0.5 font-normal text-black'
+                    type="password"
+                    name="password"
+                    placeholder='Ilovemetal123'
+                    value={password}
+                    onChange={handleChange}
+                  />
+                </label>
+              </div>
+              {errors.password?.map((message, idx) => (
+                <p className='text-red-500' key={idx}>{message}</p>
+              ))}
 
-            {/* Image */}
-            <div className="2xl:w-10/12">
-              <img 
-                className='object-center object-cover w-full h-full' 
-                src={image} 
-                alt="Live band performing in rehearsal room and jumping in the air"
-              />
-            </div>
+              <div className='flex flex-col sm:flex-row items-center justify-between text-white mt-2'>
+                <p className='mb-1 text-base md:text-lg'>Don't have an account?
+                  <Link className="text-base md:text-lg text-center ml-1 sm:ml-1.5 hover:underline underline-offset-4 decoration-2 text-amber-500" to="/signup">Sign Up</Link>
+                </p>
+
+                {/* Submit Button */}
+                <button
+                  type="submit"
+                  className='mt-2 sm:mt-0 bg-amber-500 px-3 py-2 font-bold hover:scale-105 hover:bg-transparent border-2 border-amber-500'
+                >
+                  Sign in
+                </button>
+              </div>
+              {errors.non_field_errors?.map((message, idx) => (
+                <p className='text-red-500' key={idx}>{message}</p>
+              ))}
+            </form>
+          </div>
+
+          {/* Image */}
+          <div className="2xl:w-10/12">
+            <img
+              className='object-center object-cover w-full h-full'
+              src={image}
+              alt="Live band performing in rehearsal room and jumping in the air"
+            />
           </div>
         </div>
-
+      </div>
     </div>
   )
 }
--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Link, useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import image from '../../assets/gear-addict-band-jump.jpg';
 import axios from 'axios';
@@ -7,16 +7,15 @@ import { useRedirect } from '../../hooks/useRedirect';
 
 const SignUpForm = () => {
   useRedirect('loggedIn');
-  
+
   const [signUpData, setSignUpData] = useState({
     username: '',
     password1: '',
     password2: '',
   });
+
   const { username, password1, password2 } = signUpData;
-
   const [errors, setErrors] = useState({});
-
   const history = useHistory();
 
   const handleChange = (event) => {
@@ -31,7 +30,7 @@ const SignUpForm = () => {
     try {
       await axios.post('/dj-rest-auth/registration/', signUpData);
       history.push('/signin');
-    } catch(err) {
+    } catch (err) {
       setErrors(err.response?.data);
       // console.log(err.response?.data);
     }
@@ -40,101 +39,106 @@ const SignUpForm = () => {
   return (
     <div>
 
-        {/* Hero */}
-        <Hero title='Sign up'/>
+      {/* Hero */}
+      <Hero title='Sign up' />
 
-        {/* Seperator */}
-        <div className='h-1 bg-amber-400'></div>
+      {/* Seperator */}
+      <div className='h-1 bg-amber-400'></div>
 
-        {/* Form Section */}
-        <div className='px-5 py-12 lg:py-24  bg-slate-100'>
-          <div className='container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-7'>
+      {/* Form Section */}
+      <div className='px-5 py-12 lg:py-24  bg-slate-100'>
+        <div className='container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-7'>
 
-            {/* Sign Up Form */}
-            <div className="2xl:w-10/12 2xl:ml-auto">
-              <form 
-                className='bg-zinc-800 h-full pt-10 pb-8 px-8 md:pt-16 md:pb-14 md:px-16 lg:px-12 xl:px-16 border-2 border-amber-400 shadow-xl text-left flex flex-col justify-center gap-7'
-                onSubmit={handleSubmit}
-              >
-                <div>
-                  <label className='flex flex-col sm:flex-row justify-between items-center text-white font-bold'>
-                    Username:
-                    <input 
-                      className='mt-1 sm:mt-0 border-zinc-400 border-2 w-full sm:w-7/12 px-2 py-0.5 font-normal text-black'
-                      type="text"
-                      name="username"
-                      placeholder='MrMetalHead'
-                      value={username}
-                      onChange={handleChange}
-                    />
-                  </label>
-                </div>
-                {errors.username?.map((message, idx) => 
-                  <p className='text-red-500' key={idx}>{message}</p>
-                )}
+          {/* Sign Up Form */}
+          <div className="2xl:w-10/12 2xl:ml-auto">
+            <form
+              className='bg-zinc-800 h-full pt-10 pb-8 px-8 md:pt-16 md:pb-14 md:px-16 lg:px-12 xl:px-16 border-2 border-amber-400 shadow-xl text-left flex flex-col justify-center gap-7'
+              onSubmit={handleSubmit}
+            >
 
-                <div>
-                  <label className='flex flex-col sm:flex-row justify-between items-center text-white font-bold'>
-                    Password:
-                    <input
-                      className='mt-1 sm:mt-0 border-zinc-400 border w-full sm:w-7/12 px-2 py-0.5 font-normal text-black'
-                      type="password"
-                      name="password1"
-                      placeholder='Ilovemetal123'
-                      value={password1}
-                      onChange={handleChange}
-                    />
-                  </label>
-                </div>
-                {errors.password1?.map((message, idx) => 
-                  <p className='text-red-500' key={idx}>{message}</p>
-                )}
+              {/* Username */}
+              <div>
+                <label className='flex flex-col sm:flex-row justify-between items-center text-white font-bold'>
+                  Username:
+                  <input
+                    className='mt-1 sm:mt-0 border-zinc-400 border-2 w-full sm:w-7/12 px-2 py-0.5 font-normal text-black'
+                    type="text"
+                    name="username"
+                    placeholder='MrMetalHead'
+                    value={username}
+                    onChange={handleChange}
+                  />
+                </label>
+              </div>
+              {errors.username?.map((message, idx) =>
+                <p className='text-red-500' key={idx}>{message}</p>
+              )}
 
-                <div>
-                  <label className='flex flex-col sm:flex-row justify-between items-center text-white font-bold'>
-                    Confirm Password:
-                    <input
-                      className='mt-1 sm:mt-0 border-zinc-400 border w-full sm:w-7/12 px-2 py-0.5 font-normal text-black'
-                      type="password"
-                      name="password2"
-                      placeholder='Ilovemetal123'
-                      value={password2}
-                      onChange={handleChange}
-                    />
-                  </label>
-                </div>
-                {errors.password2?.map((message, idx) => 
-                  <p className='text-red-500' key={idx}>{message}</p>
-                )}
+              {/* Password */}
+              <div>
+                <label className='flex flex-col sm:flex-row justify-between items-center text-white font-bold'>
+                  Password:
+                  <input
+                    className='mt-1 sm:mt-0 border-zinc-400 border w-full sm:w-7/12 px-2 py-0.5 font-normal text-black'
+                    type="password"
+                    name="password1"
+                    placeholder='Ilovemetal123'
+                    value={password1}
+                    onChange={handleChange}
+                  />
+                </label>
+              </div>
+              {errors.password1?.map((message, idx) =>
+                <p className='text-red-500' key={idx}>{message}</p>
+              )}
 
-                <div className='flex flex-col sm:flex-row items-center justify-between text-white mt-2'>
-                  <p className='mb-1 text-base md:text-lg'>Already have an account? 
-                    <Link className="text-base md:text-lg text-center ml-1 sm:ml-1.5 hover:underline underline-offset-4 decoration-2 text-amber-500" to="/signin">Sign In</Link>
-                  </p>
-                  <button 
-                    type="submit"
-                    className='mt-2 sm:mt-0 bg-amber-500 px-3 py-2 font-bold hover:scale-105 hover:bg-transparent border-2 border-amber-500'
-                  >
-                    Sign up
-                  </button>
-                </div>
-                {errors.non_field_errors?.map((message, idx) => 
-                  <p className='text-red-500' key={idx}>{message}</p>
-                )}
-              </form>
-            </div>
+              {/* Password 2 */}
+              <div>
+                <label className='flex flex-col sm:flex-row justify-between items-center text-white font-bold'>
+                  Confirm Password:
+                  <input
+                    className='mt-1 sm:mt-0 border-zinc-400 border w-full sm:w-7/12 px-2 py-0.5 font-normal text-black'
+                    type="password"
+                    name="password2"
+                    placeholder='Ilovemetal123'
+                    value={password2}
+                    onChange={handleChange}
+                  />
+                </label>
+              </div>
+              {errors.password2?.map((message, idx) =>
+                <p className='text-red-500' key={idx}>{message}</p>
+              )}
 
-            {/* Image */}
-            <div className="2xl:w-10/12">
-              <img 
-                className='object-center object-cover w-full h-full' 
-                src={image} 
-                alt="Live band performing in rehearsal room and jumping in the air"
-              />
-            </div>
+              <div className='flex flex-col sm:flex-row items-center justify-between text-white mt-2'>
+                <p className='mb-1 text-base md:text-lg'>Already have an account?
+                  <Link className="text-base md:text-lg text-center ml-1 sm:ml-1.5 hover:underline underline-offset-4 decoration-2 text-amber-500" to="/signin">Sign In</Link>
+                </p>
+
+                {/* Submit Button */}
+                <button
+                  type="submit"
+                  className='mt-2 sm:mt-0 bg-amber-500 px-3 py-2 font-bold hover:scale-105 hover:bg-transparent border-2 border-amber-500'
+                >
+                  Sign up
+                </button>
+              </div>
+              {errors.non_field_errors?.map((message, idx) =>
+                <p className='text-red-500' key={idx}>{message}</p>
+              )}
+            </form>
+          </div>
+
+          {/* Image */}
+          <div className="2xl:w-10/12">
+            <img
+              className='object-center object-cover w-full h-full'
+              src={image}
+              alt="Live band performing in rehearsal room and jumping in the air"
+            />
           </div>
         </div>
-
+      </div>
     </div>
   )
 }
