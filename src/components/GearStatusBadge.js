5fe1f174e9eee58f18da3cd3bcd0b5d36ef5706f
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-23 10:47:23 +0000

    Add indentation to gear status badge component

--- src/components/GearStatusBadge.js
+++ src/components/GearStatusBadge.js
@@ -9,17 +9,17 @@ const GearStatusBadge = (props) => {
 
     return (
         <>
-            {repair && 
+            {repair &&
                 <div className="rounded-full h-10 w-10 bg-white flex items-center justify-center">
                     <i className="fa-solid fa-hammer text-2xl text-red-600"></i>
                 </div>
             }
-            {sale && 
+            {sale &&
                 <div className="rounded-full h-10 w-10 bg-white flex items-center justify-center">
                     <i className="fa-regular fa-money-bill-wave text-2xl text-green-700"></i>
                 </div>
             }
-            {insured && 
+            {insured &&
                 <div className="rounded-full h-10 w-10 bg-white flex items-center justify-center">
                     <i className="fa-solid fa-shield-check text-2xl text-blue-600"></i>
                 </div>
