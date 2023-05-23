3fcee8c2e92d91501705eeb0cf7bbb113730d95b
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-23 10:20:02 +0000

    Chnage icon seperator icon colour

--- src/components/IconSeperator.js
+++ src/components/IconSeperator.js
@@ -3,7 +3,7 @@ import React from 'react'
 const IconSeperator = () => {
   return (
     <div className='h-1 bg-amber-400 flex items-center justify-center'>
-        <i className="fa-solid fa-fire-flame text-3xl bg-amber-500 text-yellow-100 rounded-full h-10 w-10 flex items-center justify-center"></i>
+        <i className="fa-solid fa-fire-flame text-3xl bg-amber-500 text-white rounded-full h-10 w-10 flex items-center justify-center"></i>
     </div>
   )
 }
