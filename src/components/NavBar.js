948152f6207748dc39c37232abbe8ed5fc197421
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-05-23 08:29:58 +0000

    Minor responsive changes to nav bar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -39,6 +39,7 @@ const NavBar = () => {
             onClick={handleClick}
             to="/gear/create"
             className='text-white flex items-center hover:text-amber-400 hover:scale-105'
+            activeClassName={styles.Active}
         >
             <i className="fa-solid fa-circle-plus text-2xl mr-2"></i>
             Add gear
@@ -50,6 +51,7 @@ const NavBar = () => {
             onClick={handleClick}
             to="/rig/create"
             className='text-white flex items-center hover:text-amber-400 hover:scale-105'
+            activeClassName={styles.Active}
         >
             <i className="fa-solid fa-circle-plus text-2xl mr-2"></i>
             Add rig
@@ -160,12 +162,12 @@ const NavBar = () => {
                             <img src={logo} alt="Gear Addict Logo" className='object-center object-contain h-full w-full' />
                         </div>
                     </NavLink>
-                    <div className='flex items-center gap-8 tracking-wide text-white'>
+                    <div className='lg:flex items-center gap-8 tracking-wide text-white hidden'>
                         {currentUser && addGearIcon}
                         {currentUser && addRigIcon}
                     </div>
                 </div>
-                <div className={`transition-all z-50 duration-500 overflow-hidden flex lg:p-0 absolute bg-zinc-800 top-[90px] right-0 left-0 flex-col lg:static lg:flex-row items-center lg:max-h-none ${isActive ? 'max-h-[999px]' : 'max-h-0'}`}>
+                <div className={`transition-all z-50 duration-500 overflow-hidden border-amber-400 lg:border-0 flex lg:p-0 absolute bg-zinc-800 top-[88px] right-0 left-0 flex-col lg:static lg:flex-row items-center lg:max-h-none ${isActive ? 'max-h-[999px] border-b-4' : 'max-h-0 border-0'}`}>
                     <nav className='text-white lg:mr-8 p-5'>
                         <ul className='flex flex-col lg:flex-row items-center gap-5 tracking-wide'>
                             <li>
@@ -193,6 +195,12 @@ const NavBar = () => {
                                 </NavLink>
                             </li>
                             {currentUser ? loggedInIcons : loggedOutIcons}
+                            <li className='lg:hidden'>
+                                {currentUser && addGearIcon}
+                            </li>
+                            <li className='lg:hidden'>
+                                {currentUser && addRigIcon}
+                            </li>
                         </ul>
                     </nav>
                     <div className='text-white hidden lg:block'>
@@ -200,7 +208,7 @@ const NavBar = () => {
                     </div>
                 </div>
                 <div className='flex lg:hidden items-center'>
-                    <div className='text-white mr-8'>
+                    <div className='text-white mr-5 md:mr-8'>
                         {currentUser && profileIcon}
                     </div>
                     <button 
